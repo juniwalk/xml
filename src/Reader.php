1f6e7cdf5319c58235ae8b401369a6cf75a8745b
Martin Procházka <juniwalk@outlook.cz>
2023-06-12 14:50:28 +0200

    Added attribute for future return type change

--- src/Reader.php
+++ src/Reader.php
@@ -53,6 +53,7 @@ final class Reader extends XmlReader
 	/**
 	 * @throws XmlException
 	 */
+	#[\ReturnTypeWillChange]
 	public function expand($baseNode = null)
 	{
 		$result = @parent::expand($baseNode);
